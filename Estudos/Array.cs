ea45f3bfb5ece405710839eb5c08d790c97ef1f4
Rafael Jageneski <rafa.edjnonato@gmail.com>
2022-07-06 23:01:47 -0300

    test

---
+++ Estudos/Array.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
