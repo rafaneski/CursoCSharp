69315ab5deffb668acb46fa3f89937561fb6f73e
Rafael Jageneski <rafa.edjnonato@gmail.com>
2022-07-06 23:14:22 -0300

    commit

--- Estudos/Arrays/Arrays/Program.cs
+++ Estudos/Arrays/Arrays/Program.cs
@@ -1,16 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Array
+namespace Arrays
 {
-    internal class Array
+    internal class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
